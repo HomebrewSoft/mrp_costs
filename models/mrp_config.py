54b5a78d2d0e34682efae8b4f54fdd551c505f14
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2020-03-24 20:48:35 -0600

    [IMP] cost_rent

--- models/mrp_config.py
+++ models/mrp_config.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from odoo import _, api, fields, models, fields
 
+
 class MRPConfigSettings(models.TransientModel):
     _inherit = 'mrp.config.settings'
 
     cost_rent = fields.Float(
-        # related='company_id.cost_rent',
+        related='company_id.cost_rent',
     )
